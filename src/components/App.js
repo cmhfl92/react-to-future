0d988e522581f3eafbe11ff401735cc6f0c2ab40
Christa Hegedus <Christa.Hegedus1@gmail.com>
2016-12-13 14:44:03 -0500

    Revert ""Hello, App App!""
    This reverts commit 8de49f31c0f7dad1d4828cb6e0d2b30e89b14c4a.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,92 @@
 import React, { Component } from 'react'
+import Slider from './Slider.js'
 
 class App extends Component {
 
+  constructor () {
+    super()
+    this.state = {
+      hue: 120,
+      saturation: 100,
+      lightness: 50,
+      alpha: 1
+    }
+  }
+
+  updateHue (hue) {
+    this.setState({
+      hue: hue
+    })
+  }
+
   render () {
     return <div>
-      <h1>Hello, World!</h1>
+      <h1>HSLA Color Picker</h1>
+      <figure>
+        <div />
+      </figure>
+      <ul>
+        <li>H <input className='hue' name='hue' type='range' min='0' max='360' /> </li>
+        <li>S <input className='sat' name='sat' type='range' min='0' max='100' /> </li>
+        <li>L <input className='light' name='light' type='range' min='0' max='100' /> </li>
+        <li>A <input className='alpha' name='alpha' type='range' min='0.00' max='1.00' step='.01' /> </li>
+      </ul>
+      <section>
+        <h3>hsla(130, 50%, 50%, .5)</h3>
+      </section>
     </div>
   }
 }
+// JS
+// let hue = 128
+// let sat = 100
+// let light = 50
+// let alpha = 1
+// // hsla(128, 100%, 50%, 1)
+// const getHSL = () => {
+//   return `hsla(${hue}, ${sat}%, ${light}%, ${alpha})`
+// }
+//
+// const main = () => {
+//   const colorsquare = document.querySelector('.colorsquare')
+//   const hueInput = document.querySelector('input[name=hue]')
+//   hueInput.addEventListener('input', () => {
+//     hue = hueInput.value
+//     colorsquare.style.backgroundColor = getHSL()
+//     document.querySelector('h3').textContent = getHSL()
+//   })
+//   const satInput = document.querySelector('input[name=sat]')
+//   satInput.addEventListener('input', () => {
+//     sat = satInput.value
+//     colorsquare.style.backgroundColor = getHSL()
+//     document.querySelector('h3').textContent = getHSL()
+//   })
+//   const lightInput = document.querySelector('input[name=light]')
+//   lightInput.addEventListener('input', () => {
+//     light = lightInput.value
+//     colorsquare.style.backgroundColor = getHSL()
+//     document.querySelector('h3').textContent = getHSL()
+//   })
+//   const alphaInput = document.querySelector('input[name=alpha]')
+//   alphaInput.addEventListener('input', () => {
+//     alpha = alphaInput.value
+//     colorsquare.style.backgroundColor = getHSL()
+//     document.querySelector('h3').textContent = getHSL()
+//   })
+//
+//   document.getElementById('sat')
+//   document.getElementById('light')
+// }
+//
+// document.addEventListener('DOMContentLoaded', main)
+
+// html
+//     <section>
+//     <div class="colorsquare">
+//       <h1></h1>
+//     </div>
+//   </section>
+//   </body>
+// </html>
 
 export default App
